15bd22521ffd911126a0d560f1f66cec123ce82b
Chris <christinittyofc@gmail.com>
2020-02-15 16:11:21 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,124 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//redux
+import { createStore   } from 'redux';
+import { Provider,connect } from 'react-redux';
+
+
+//state in the app is only a message array
+//props will only have a message property
+const mapStateToProps = (state) => {
+  return {
+    messages: state
+  };
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (newMessage) => {
+      return dispatch(addMessage(newMessage));
+    }
+  }
+}
+
+//action is to add a message
+
+const ADD = 'ADD';
+
+function addMessage(message) {
+    return {
+        type: ADD,
+        message: message
+    };
+};
+
+//reducer concats the message to an array in the state
+
+function messageReducer(state=[],action){
+
+    //concat message if type == ADD
+    if(action.type === ADD) {
+        return [...state, ...[action.message]];
+    } else {
+        return state;
+    };
+};
+
+
+// react app
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      //messages: []
+    }
+    this.handleChange =  this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  submitMessage(e){
+    this.props.submitNewMessage(this.state.input);
+    this.setState({
+      //messages: [...this.state.messages, ...[this.state.input]],
+      input: ''
+    });
+  }
+
+
+  render (){
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h2>Type in a new Note:</h2>
+        { /* render an input, button, and ul here */ }
+        <div className="form-group">
+        <input value={this.state.input} onChange={this.handleChange}/>
+        </div>
+        <div className="form-group">
+        <button className="btn btn-primary" type="submit" onClick={this.submitMessage}>Add Note</button>
+        </div>
+        <ul>{this.props.messages.map((message) => (<li>{message}</li>))}</ul>
+        </header>
+      </div>
+    );
+  }
+ 
+}
+
+//redux store that stores the state of the app
+
+const store = createStore(messageReducer);
+
+//container that connects redux store to the react app
+
+const Container  = connect(mapStateToProps, mapDispatchToProps)(App);
+
+//redux apWrapper that renders the app as a child of redux components
+class AppWrapper extends React.Component {
+
+    constructor(props){
+      super(props);
+      
+}  
+render(){
+    return(
+      <Provider store={store}>
+        <Container />
+      </Provider>  
+    )
+  }
+
+}
+
+export default AppWrapper;
